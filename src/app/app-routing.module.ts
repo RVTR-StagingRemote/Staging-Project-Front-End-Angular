3e6326fa1eade472b02e0b7316eb218b0738ef53
Rearles <53582049+Rearles@users.noreply.github.com>
2021-12-01 12:20:35 -0700

    set up routes for resetting login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResetloginComponent } from './components/resetlogin/resetlogin.component';
 import { AboutComponent } from './pages/about/about.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -7,7 +8,8 @@ import { RegisterComponent } from './pages/register/register.component';
 const routes: Routes = [
   {path: '', component: LandingComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'register', component: RegisterComponent}
+  { path: 'register', component: RegisterComponent },
+  { path: 'resetlogin', component: ResetloginComponent }
 ];
 
 @NgModule({
