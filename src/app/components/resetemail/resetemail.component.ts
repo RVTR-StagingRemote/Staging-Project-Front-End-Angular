5b4998790c56040f58865fc79326c666cc28a57c
Rearles <53582049+Rearles@users.noreply.github.com>
2021-12-06 11:12:14 -0700

    implement bootstrap

--- src/app/components/resetemail/resetemail.component.ts
+++ src/app/components/resetemail/resetemail.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-resetemail',
   templateUrl: './resetemail.component.html',
   styleUrls: ['./resetemail.component.css']
 })
+  /*
+    As a user I want to be able to reset my email if I forget it.
+    This'll be implemented fully in a later iteration of this project where we would email the person at their email to reset their email to a new email.  
+  */
 export class ResetemailComponent implements OnInit {
-
+  @Input() userData = { Firstname: '', Lastname: '', Email: '', Password: '' };
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  /*
+    This is what will be called by the form submittion in resetemail.component.html
+  */
+  resetemail(): void {
+    console.log(this.userData)
+  }
 }
